cf7c6cf43541ba55a54222ec599d8bd76d64a1cf
Anders Gilliland <lion@Edward>
2022-10-21 10:35:06 -0400

    Added comment

--- flask/api.py
+++ flask/api.py
@@ -3,7 +3,7 @@ from flask_restful import Api, Resource
 from flask_cors import CORS
   
 app =   Flask(__name__)
-CORS(app)
+CORS(app) # Potential security risk? Not sure what this is doing, but necessary for localhost
 api =   Api(app)
   
 class returnjson(Resource):

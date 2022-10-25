96c6caa7189b27303b9125372e3655ac6c4182ac
Anders Gilliland <lion@Arnold.localdomain>
2022-10-25 13:20:57 -0400

    Cleaned out extra files

---
+++ react-app/src/Api.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    data: []
+  }
+
+  // Code is invoked after the component is mounted/inserted into the DOM tree.
+  componentDidMount() {
+    const url =
+      //'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*'
+      'http://127.0.0.1:5000/'
+
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+            data: res
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state
+
+    console.log(data)
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+
+    return <ul>{result}</ul>
+  }
+}
+
+export default App
